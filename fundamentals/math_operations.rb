ccf54cd3736c109aaff4e2c489de96b78450d2c9
lucarl07 <junitinho2007@gmail.com>
2024-06-26 23:30:53 -0300

    Add math_operations.rb on fundamentals

---
+++ fundamentals/math_operations.rb
@@ -0,0 +1,19 @@
+### OPERAÇÕES MATEMÁTICAS NO RUBY - Por: lucarl07 ###
+
+# Adição
+puts 1 + 7 # Ei, não tenta fazer 0.1 + 0.7...
+
+# Subtração
+puts 9 - 6
+
+# Multiplicação
+puts 10 * 10
+
+# Divisão
+puts 25.0 / 5.0 # Para sempre obter resultados exatos, é aconselhável deixar o dividendo no formato decimal.
+
+# Módulo
+puts 23 % 3
+
+# Expoente
+puts 4 ** 3
