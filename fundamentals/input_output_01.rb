072245c0ca682cb166d03af5b6f949526b87c39a
lucarl07 <junitinho2007@gmail.com>
2024-06-27 00:31:18 -0300

    Add commentary on input_output_01.rb

--- fundamentals/input_output_01.rb
+++ fundamentals/input_output_01.rb
@@ -1,10 +1,15 @@
 ### ENTRADA E SAÍDA (I/O) NO RUBY | Por: lucarl07 ###
+=begin
+  Muitas vezes, no nosso programa, precisamos obter dados fornecidos pelo usuário e construir uma nova informação, dado
+  ou cálculo. Por meio do terminal com suporte à Ruby, podemos fazer isso através do comando de entrada "print", e o de
+  saída já visto antes, "puts".
+=end
 
 # Entrada de dados:
 print 'Digite o seu nome: '
 
 # Recebendo uma entrada do terminal:
-name = gets.chomp
+name = gets.chomp # O método "chomp" permite a remoção de linhas vazias, algo que ocorre ao obter dados via terminal.
 
 # Repetindo os 2 primeiros processos:
 print 'Digite o seu gênero (F/M): '
